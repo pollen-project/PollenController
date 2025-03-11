6ef3dbe09d0eab12c221fd5ad0c686643a32315a
MagnusTrojahn <90682418+MagnusTrojahn@users.noreply.github.com>
2025-03-11 11:04:06 +0100

    Update main_controller.py

--- RaspberryPi_stepper_tests/main_controller.py
+++ RaspberryPi_stepper_tests/main_controller.py
@@ -1,13 +1,30 @@
 # controller.py
 import interface
 
-def main():
-    print()
-
 def setup():
-    interface.setup()
+    return interface.setup()
+
+def focus_home():
+    interface.focus_home()
+
+def focus():
+    interface.focus()
+
+def main():
+    camera = setup()
+
+    while True:
+        command = input("Enter command: ").strip().lower()
+
+        if command == "home":
+            focus_home()
+        elif command == "focus":
+            focus()
+        elif command == "exit":
+            print("Exiting...")
+            break
+        else:
+            print("Unknown command. Try 'run focus' or 'exit'.")
 
 if __name__ == "__main__":
-    setup()
-
-    
+    main()
